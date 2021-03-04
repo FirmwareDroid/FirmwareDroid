4ce46c42f4d53bf61aa632496dc62673705270cb
Thomas Sutter <suth@zhaw.ch>
2021-03-04 11:37:08 +0100

    Add detached mode for running script.

--- 01_run.sh
+++ 01_run.sh
@@ -15,11 +15,13 @@ where:
     -h  show this help text
     -e  set the environment (default: 0) (options: 0 = development, 1 = testing, 2 = production)
     -r  set the running mode (default: 0) (options: 0 = run, 1 = build intermediate images and run, 2 = build complete images and run)
+    -d  run docker-compose detached
 "
 
 environment=0
 running_mode=0
-while getopts 'he:r:' option; do
+detached_mode=0
+while getopts 'dhe:r:' option; do
   case "$option" in
     h)  echo "$usage"
         exit 0
@@ -37,6 +39,9 @@ while getopts 'he:r:' option; do
           printf "argument is not a number for -%s\n" "$OPTARG" >&2
           exit 1
        fi
+       ;;
+    d) detached_mode = 1
+        echo "Run detached"
        ;;
     :) printf "missing argument for -%s\n" "$OPTARG" >&2
        echo "$usage" >&2
@@ -78,5 +83,9 @@ compose_path=$PWD"/"$compose_file
 echo "########################################
 Starting FirmwareDroid now
 #######################################"
-# Run app
-docker-compose -f $PWD"/docker-compose.yml" -f $compose_path up
+# Run containers
+if [ $detached_mode == 0 ]; then
+  docker-compose -f $PWD"/docker-compose.yml" -f $compose_path up
+else
+  docker-compose -f $PWD"/docker-compose.yml" -f $compose_path up -d
+fi
