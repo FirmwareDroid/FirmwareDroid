7326f318ca1c4a1d0fd5e0b0e4179020d87b3d21
Thomas Sutter <suth@zhaw.ch>
2021-07-09 14:36:22 +0200

    Change firmware import queue.

--- model/QuarkEngineReport.py
+++ model/QuarkEngineReport.py
@@ -3,8 +3,10 @@ from model import AndroidApp
 import datetime
 from flask_mongoengine import Document
 
+
 class QuarkEngineReport(Document):
     report_date = DateTimeField(required=True, default=datetime.datetime.now)
     android_app_id_reference = LazyReferenceField(AndroidApp, reverse_delete_rule=CASCADE, required=True)
     quark_engine_version = StringField(required=True)
     scan_results = DictField(required=True)
+
