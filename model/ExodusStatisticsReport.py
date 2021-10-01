2c2f24406ee62db47b9762544c8567dc9ae1ba0a
Thomas Sutter <suth@zhaw.ch>
2021-10-01 08:32:09 +0200

    Update Exodus reports with comments. Add new Exodus statics for number of apps with/without trackers.

--- model/ExodusStatisticsReport.py
+++ model/ExodusStatisticsReport.py
@@ -1,6 +1,8 @@
-from mongoengine import DictField
+from mongoengine import DictField, LongField
 from model import StatisticsReport
 
 
 class ExodusStatisticsReport(StatisticsReport):
     tracker_count_dict = DictField(required=False)
+    number_of_apps_with_no_trackers = LongField(required=False)
+    number_of_apps_with_trackers = LongField(required=False)
--- scripts/statistics/reports/exodus/exodus_statistics.py
+++ scripts/statistics/reports/exodus/exodus_statistics.py
@@ -1,6 +1,4 @@
 import logging
-
-from bson import ObjectId
 from model import ExodusReport
 from model.ExodusStatisticsReport import ExodusStatisticsReport
 from scripts.rq_tasks.flask_context_creator import create_app_context
@@ -32,81 +30,75 @@ def create_exodus_statistics_report(android_app_id_list, report_name):
 
 
 def add_exodus_statistics(statistics_report, report_objectid_list):
-    #firmware_by_vendor_and_version_dict = get_firmware_by_vendor_and_version(android_app_objectid_list)
-    #app_by_vendor_and_version_dict = get_apps_by_vendor_and_version(firmware_by_vendor_and_version_dict)
-    #tracker_frequency_by_fw_version_dict = get_tracker_frequency_by_fw_version(app_by_vendor_and_version_dict)
-    #statistics_report.tracker_frequency_by_fw_version_dict = tracker_frequency_by_fw_version_dict
+    """
+    Gets the various statistic data for exodus and saves it to the database.
+    :param statistics_report: class:'ExodusStatisticsReport'
+    :param report_objectid_list: list(ObjecIds) - list of class:'ExodusReport' objectids.
+    """
     statistics_report.tracker_count_dict = get_tracker_frequency(report_objectid_list)
+    statistics_report.number_of_apps_with_trackers = get_number_of_apps_by_tracker_number(report_objectid_list,
+                                                                                          tracker_count=0,
+                                                                                          comparison_sign="$gt")
+    statistics_report.number_of_apps_with_no_trackers = get_number_of_apps_by_tracker_number(report_objectid_list,
+                                                                                             tracker_count=0,
+                                                                                             comparison_sign="$eq")
     statistics_report.save()
     logging.info(f"Added tracker by version and vendor statistics to Exodus!")
-
-
-def get_tracker_frequency_by_fw_version(app_by_vendor_and_version_dict):
-    tracker_frequency_by_fw_version_dict = {}
-    for os_vendor, os_version_dict in app_by_vendor_and_version_dict.items():
-        if str(os_vendor) not in tracker_frequency_by_fw_version_dict:
-            tracker_frequency_by_fw_version_dict[str(os_vendor)] = {}
-        for os_version, android_app_id_list in os_version_dict.items():
-            android_objectid_list = []
-            for android_app_id in android_app_id_list:
-                android_objectid_list.append(ObjectId(android_app_id.pk))
-            tracker_frequency_by_fw_version_dict[str(os_vendor)][str(os_version)] = \
-                get_tracker_frequency(android_objectid_list)
-    return tracker_frequency_by_fw_version_dict
 
 
 def get_tracker_frequency(report_objectid_list):
     """
     Gets the count of AD-Trackers. For each tracker the sum is aggregated over all apps.
+    :param report_objectid_list: list(ObjecIds) - list of class:'ExodusReport' objectids.
     :return: List(dict(str, int)) - List(dict("AD-Tracker-Name", Tracker-Count))
     """
     tracker_dict = {}
     chunk_list = [report_objectid_list[x:x + 1000] for x in range(0, len(report_objectid_list), 1000)]
     for chunk in chunk_list:
         command_cursor = ExodusReport.objects(pk__in=chunk).aggregate([
-          {
-            "$match": {
-              "results.trackers": {
-                "$ne": [
+            {
+                "$match": {
+                    "results.trackers": {
+                        "$ne": [
 
-                ]
-              }
-            }
-          },
-          {
-            "$project": {
-              "results.trackers": 1
-            }
-          },
-          {
-            "$unwind": "$results.trackers"
-          },
-          {
-            "$group": {
-              "_id": "$results.trackers.name",
-              "count": {
-                "$sum": 1
-              }
-            }
-          },
-          {
-            "$group": {
-              "_id": "null",
-              "counts": {
-                "$push": {
-                  "k": "$_id",
-                  "v": "$count"
+                        ]
+                    }
                 }
-              }
+            },
+            {
+                "$project": {
+                    "results.trackers": 1
+                }
+            },
+            {
+                "$unwind": "$results.trackers"
+            },
+            {
+                "$group": {
+                    "_id": "$results.trackers.name",
+                    "count": {
+                        "$sum": 1
+                    }
+                }
+            },
+            {
+                "$group": {
+                    "_id": "null",
+                    "counts": {
+                        "$push": {
+                            "k": "$_id",
+                            "v": "$count"
+                        }
+                    }
+                }
+            },
+            {
+                "$replaceRoot": {
+                    "newRoot": {
+                        "$arrayToObject": "$counts"
+                    }
+                }
             }
-          },
-          {
-            "$replaceRoot": {
-              "newRoot": {
-                "$arrayToObject": "$counts"
-              }
-            }
-          }
         ], allowDiskUse=True)
 
         for document in command_cursor:
@@ -117,6 +109,44 @@ def get_tracker_frequency(report_objectid_list):
                 else:
                     tracker_dict[tracker_name] = count
     return tracker_dict
+
+
+def get_number_of_apps_by_tracker_number(report_objectid_list, tracker_count, comparison_sign):
+    """
+    Count the number of apps that have a specific count of trackers.
+    :param comparison_sign: string - MongoDB aggregation string for comparison. Examples: "$gt", "$eq", "$lt"
+    :param tracker_count: int - condition for counting the number of apps if a specific number of trackers are detected.
+    :param report_objectid_list: list(ObjecIds) - list of class:'ExodusReport' objectids.
+    :return: int
+    """
+    count = 0
+    chunk_list = [report_objectid_list[x:x + 1000] for x in range(0, len(report_objectid_list), 1000)]
+    for chunk in chunk_list:
+        command_cursor = ExodusReport.objects(pk__in=chunk).aggregate([
+            {
+                "$project": {
+                    "count": {
+                        f"{comparison_sign}": [
+                            {
+                                "$size": "$results.trackers"
+                            },
+                            tracker_count
+                        ]
+                    }
+                }
+            },
+            {
+                "$match": {
+                    "count": True
+                }
+            },
+            {
+                "$count": "count"
+            }
+        ], allowDiskUse=True)
+        for document in command_cursor:
+            count += document.get("count")
+    return count
 
 
 def create_empty_exodus_statistics_report(report_name, report_count, android_app_id_list, android_app_reference_file):
