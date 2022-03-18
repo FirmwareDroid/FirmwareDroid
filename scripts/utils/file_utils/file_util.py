6f3bdbe3ab953cdc3313ebf5d428b419f5605b17
Thomas Sutter <suth@zhaw.ch>
2022-03-18 11:17:41 +0100

    Fix delete method bug.

--- scripts/utils/file_utils/file_util.py
+++ scripts/utils/file_utils/file_util.py
@@ -33,15 +33,15 @@ def delete_files_in_folder(folder):
     """
     Deletes all files in the given folder
     """
-    for filename in os.listdir(folder):
-        file_path = os.path.join(folder, filename)
-        try:
+    try:
+        for filename in os.listdir(folder):
+            file_path = os.path.join(folder, filename)
             if os.path.isfile(file_path) or os.path.islink(file_path):
                 os.unlink(file_path)
             elif os.path.isdir(file_path):
                 shutil.rmtree(file_path)
-        except Exception as e:
-            print('Failed to delete %s. Reason: %s' % (file_path, e))
+    except Exception as e:
+        print(f'Failed to delete %s. Reason: {e}')
 
 
 def delete_folder(dir_path):
