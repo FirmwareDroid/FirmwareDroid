662ab1d650385fd17e5c020740cd08d291f3fc20
Thomas Sutter <sutter.thomas@protonmail.com>
2023-12-13 14:19:47 +0100

    Add code comments for AndroidAppSchema.py

--- source/api/v2/schema/AndroidAppSchema.py
+++ source/api/v2/schema/AndroidAppSchema.py
@@ -20,7 +20,6 @@ class AndroidAppQuery(graphene.ObjectType):
                                      name="android_app_list")
     android_app_id_list = graphene.List(graphene.String,
                                         object_id_list=graphene.List(graphene.String),
-                                        document_type=graphene.String(),
                                         name="android_app_id_list")
 
     @staticmethod
@@ -42,10 +41,30 @@ class AndroidAppQuery(graphene.ObjectType):
 
     @superuser_required
     def resolve_android_app_list(self, info, **kwargs):
+        """
+        Gets a list of class:'AndroidApp' documents based on the given document type. In case the "AndroidFirmware"
+        document type is set, all apps of the specific firmware will be returned. In case "AndroidApp" is set, only
+        the selected AndroidApp documents will be returned.
+
+        :param kwargs:
+          - document_type: str - The document type to get the apps from. Either, "AndroidApp" or "AndroidFirmware"
+          - object_id_list: list(str) - document-ids of class:'AndroidApp' or class:'AndroidFirmware'
+
+        :return: list(str) - returns a list of class:'AndroidApp' object-ids.
+        """
         return AndroidAppQuery.get_android_app_list(**kwargs)
 
     @superuser_required
-    def resolve_android_app_id_list(self, info, **kwargs):
+    def resolve_android_app_id_list(self, info, object_id_list):
+        """
+        Gets a list of class:'AndroidApp' object-ids from the given class:'AndroidFirmware' object-ids.
+        All apps of the specific firmware will be returned.
+
+        :param object_id_list: document-ids of class:'AndroidFirmware' to fetch the document id's from.
+
+        :return: list(str) - returns a list of class:'AndroidApp' object-ids.
+        """
+        kwargs = {"object_id_list": object_id_list, "document_type": "AndroidFirmware"}
         android_app_list = AndroidAppQuery.get_android_app_list(**kwargs)
         app_id_list = []
         for android_app in android_app_list:
