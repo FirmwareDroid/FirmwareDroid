78a95c95c663bf28b5c827be9930b1ae74f74bcc
Thomas Sutter <sutter.thomas@protonmail.com>
2024-06-13 11:50:06 +0200

    Update AECS shared library module builder to include subfolders

--- source/dynamic_analysis/emulator_preparation/aosp_shared_library_builder.py
+++ source/dynamic_analysis/emulator_preparation/aosp_shared_library_builder.py
@@ -8,6 +8,50 @@ from dynamic_analysis.emulator_preparation.templates.shared_library_module_templ
     ANDROID_MK_SHARED_LIBRARY_TEMPLATE, ANDROID_BP_SHARED_LIBRARY_TEMPLATE
 
 
+def is_top_folder(library_path, folder_name):
+    path_list = library_path.split(os.sep)
+    return path_list[0] == folder_name
+
+
+def get_subfolders(library_path, top_folder_name):
+    """
+    Get the subfolders after a specific top folder.
+
+    :param library_path: str - path to the library.
+    :param top_folder_name: str - name of the top folder.
+
+    :return: list(str) - list of subfolders after the folder in case there are any subfolders.
+
+    """
+    subfolders = []
+    if top_folder_name in library_path and not is_top_folder(library_path, top_folder_name):
+        path_list = library_path.split(os.sep)
+        lib64_index = path_list.index("lib64")
+        subfolders = path_list[lib64_index + 1:]
+    return subfolders
+
+
+def get_lib_local_module_path(library_path, folder_name):
+    """
+    Get the local module path for the shared library module.
+
+    :param library_path: str - path to the shared library module.
+    :param folder_name: str - name of the lib folder on Android.
+
+    :return: str - local module path for the shared library module.
+    """
+    subfolder_list = []
+    if is_top_folder(library_path, folder_name):
+        local_module_path = f"$(TARGET_OUT)/{folder_name}/"
+    else:
+        subfolder_list = get_subfolders(library_path, folder_name)
+        if len(subfolder_list) == 0:
+            local_module_path = f"$(TARGET_OUT)/{folder_name}/"
+        else:
+            local_module_path = f"$(TARGET_OUT)/{folder_name}/{os.path.join(*subfolder_list)}/"
+    return local_module_path, subfolder_list
+
+
 def create_template_string(format_name, library_path):
     """
     Creates the template string for the shared library module.
@@ -25,10 +69,9 @@ def create_template_string(format_name, library_path):
         raise NotImplementedError(f"Format name {format_name} is not supported yet.")
 
     library_name = os.path.basename(library_path)
-    local_module = library_name.replace(".so", "")
     local_src_files = library_name
+    local_module = library_name.replace(".so", "")
 
-    # Installation path on the device
     if "/app/" in library_path:
         app_name = library_path.split("/app/")[1]
         local_module_path = f"$(TARGET_OUT)/app/{app_name}/lib/$(TARGET_ARCH_ABI)/"
@@ -36,10 +79,15 @@ def create_template_string(format_name, library_path):
         app_name = library_path.split("/priv-app/")[1]
         local_module_path = f"$(TARGET_OUT)/priv-app/{app_name}/lib/$(TARGET_ARCH_ABI)/"
     elif "/lib64/" in library_path:
-        local_module_path = "$(TARGET_OUT)/lib64/"
+        local_module_path, subfolder_list = get_lib_local_module_path(library_path, "/lib64/")
+        if len(subfolder_list) > 0:
+            local_module = f"{os.path.join(*subfolder_list)}_{local_module}".replace("/", "_")
     elif "/lib/" in library_path:
-        # Emulator does not have lib folder for 32-bit libraries
-        local_module_path = "$(TARGET_OUT)/lib64/"
+        local_module_path, subfolder_list = get_lib_local_module_path(library_path, "/lib/")
+        # Android emulator uses lib64 for 64-bit libraries and does not have a lib folder for 32-bit libraries.
+        local_module_path = local_module_path.replace("lib", "lib64")
+        if len(subfolder_list) > 0:
+            local_module = f"{os.path.join(*subfolder_list)}_{local_module}".replace("/", "_")
     else:
         local_module_path = "$(TARGET_OUT)/lib64/"
 
