e742b604bf9e48cb95dc2d1121f50baedb5c008f
Thomas Sutter <sutter.thomas@protonmail.com>
2024-07-05 13:53:18 +0200

    Replace recursion with iterative extraction approach to fix pythons maximal recursion depth issue

--- source/extractor/expand_archives.py
+++ source/extractor/expand_archives.py
@@ -3,7 +3,6 @@
 # See the file 'LICENSE' for copying permission.
 import logging
 import os
-import re
 from extractor.ext4_extractor import extract_dat
 from extractor.bin_extractor.bin_extractor import extract_bin
 from extractor.nb0_extractor import extract_nb0
@@ -12,32 +11,30 @@ from extractor.unblob_extractor import unblob_extract
 from extractor.unzipper import extract_tar, extract_zip, extract_gz
 from extractor.lz4_extractor import extract_lz4
 from extractor.brotli_extractor import extract_brotli
+from collections import deque
 
 EXTRACTION_SIZE_THRESHOLD_MB = 100
-MAX_RECURSION_DEPTH = 50
+MAX_EXTRACTION_DEPTH = 20
+SUPPORTED_FILE_TYPE_REGEX = r".(zip|tar|md5|lz4|pac|nb0|bin|br|dat|tgz|gz)$"
+EXTRACT_FUNCTION_MAP_DICT = {
+    ".zip": extract_zip,
+    ".tar": extract_tar,
+    ".tgz": extract_tar,
+    ".gz": extract_gz,
+    ".md5": extract_tar,
+    ".lz4": extract_lz4,
+    ".pac": extract_pac,
+    ".nb0": extract_nb0,
+    ".bin": extract_bin,
+    ".br": extract_brotli,
+    ".dat": extract_dat,
+}
 
 
 def normalize_file_path(file_path):
     if not os.path.exists(file_path) and not file_path.startswith("/") and not file_path.startswith("./"):
         file_path = "./" + file_path
     return file_path
-
-
-def process_file(root, filename, supported_file_types_regex, depth):
-    if re.search(supported_file_types_regex, filename.lower()):
-        nested_file_path = os.path.join(root, filename)
-        nested_file_path = normalize_file_path(nested_file_path)
-
-        if os.path.exists(nested_file_path):
-            extract_archive_layer(nested_file_path, root, True, depth)
-        else:
-            logging.debug(f"Skipped file cause it no longer exists on disk: {nested_file_path}")
-
-
-def process_directory(destination_dir, supported_file_types_regex, depth):
-    for root, dirs, files in os.walk(destination_dir):
-        for filename in files:
-            process_file(root, filename, supported_file_types_regex, depth)
 
 
 def delete_file_safely(file_path):
@@ -53,67 +50,33 @@ def get_file_size_mb(file_path):
     return size_in_mb
 
 
-def extract_archive_layer(compressed_file_path,
-                          destination_dir,
-                          delete_compressed_file,
-                          unblob_depth=2,
-                          depth=0,
-                          max_rec_depth=MAX_RECURSION_DEPTH):
-    """
-    Decompress supported archive file type and its contents recursively, including nested archives files.
-    Files smaller than EXTRACTION_SIZE_THRESHOLD_MB are not extracted.
+def extract_archive_layer(compressed_file_path, destination_dir, delete_compressed_file, unblob_depth=2,
+                          max_rec_depth=MAX_EXTRACTION_DEPTH):
+    queue = deque([(compressed_file_path, 0)])  # Each item is a tuple of (path, current depth)
+    while queue:
+        current_path, current_depth = queue.popleft()
+        if current_depth >= max_rec_depth:
+            logging.warning(f"Max recursion depth reached: {max_rec_depth}")
+            continue
 
-    :param depth: int - current recursion depth.
-    :param max_rec_depth: int - maximum recursion depth.
-    :param unblob_depth: int - depth of the unblob extraction.
-    :param compressed_file_path: str - path to the compressed file.
-    :param destination_dir: str - path to extract to.
-    :param delete_compressed_file: boolean - if true, deletes the archive after it is extracted.
-
-    """
-    if depth >= max_rec_depth:
-        logging.warning(f"Max recursion depth reached: {max_rec_depth}")
-        return
-    else:
-        depth += 1
-
-    supported_file_types_regex = r".(zip|tar|md5|lz4|pac|nb0|bin|br|dat|tgz|gz)$"
-    extract_function_dict = {
-        ".zip": extract_zip,
-        ".tar": extract_tar,
-        ".tgz": extract_tar,
-        ".gz": extract_gz,
-        ".md5": extract_tar,
-        ".lz4": extract_lz4,
-        ".pac": extract_pac,
-        ".nb0": extract_nb0,
-        ".bin": extract_bin,
-        ".br": extract_brotli,
-        ".dat": extract_dat,
-    }
-
-    is_success = False
-    file_extension = os.path.splitext(compressed_file_path.lower())[1]
-
-    if file_extension in extract_function_dict:
-        extraction_function = extract_function_dict[file_extension]
-        logging.info(f"Attempt to extract: {compressed_file_path}")
-        is_success = extraction_function(compressed_file_path, destination_dir)
-    else:
-        logging.info(f"Skip file due file extension: {compressed_file_path}")
-
-    if not is_success:
-        file_size_mb = get_file_size_mb(compressed_file_path)
-        if file_size_mb > EXTRACTION_SIZE_THRESHOLD_MB:
-            logging.info(f"Changing to unblob to extract: {compressed_file_path}")
-            unblob_extract(compressed_file_path, destination_dir, unblob_depth)
+        if os.path.isdir(current_path):
+            for filename in os.listdir(current_path):
+                file_path = os.path.join(current_path, filename)
+                queue.append((file_path, current_depth + 1))
         else:
-            logging.info(f"Skip file due small size: {compressed_file_path}")
+            file_extension = os.path.splitext(current_path.lower())[1]
+            if file_extension in EXTRACT_FUNCTION_MAP_DICT:
+                logging.info(f"Attempt to extract: {current_path}")
+                is_success = EXTRACT_FUNCTION_MAP_DICT[file_extension](current_path, destination_dir)
+                if not is_success:
+                    file_size_mb = get_file_size_mb(current_path)
+                    if file_size_mb > EXTRACTION_SIZE_THRESHOLD_MB:
+                        logging.info(f"Changing to unblob to extract: {current_path}")
+                        unblob_extract(current_path, destination_dir, unblob_depth)
+                    else:
+                        logging.info(f"Skip file due small size: {current_path}")
+            else:
+                unblob_extract(current_path, destination_dir, unblob_depth)
 
-    if delete_compressed_file:
-        delete_file_safely(compressed_file_path)
-
-    process_directory(destination_dir, supported_file_types_regex, depth)
-
-
-
+            if delete_compressed_file:
+                delete_file_safely(current_path)
