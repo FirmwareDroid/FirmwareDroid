3fe627355d9e5235f5a6e51826a69cc664428cc5
Thomas Sutter <sutter.thomas@protonmail.com>
2025-01-20 14:33:45 +0100

    Adjust template for shared libraries

--- source/dynamic_analysis/emulator_preparation/templates/shared_library_module_template.py
+++ source/dynamic_analysis/emulator_preparation/templates/shared_library_module_template.py
@@ -13,19 +13,12 @@ ANDROID_MK_SHARED_LIBRARY_TEMPLATE = "LOCAL_PATH := $$(call my-dir)\n" \
                                      "\ninclude $$(PREBUILT_SHARED_LIBRARY)\n"
 
 ANDROID_BP_SHARED_LIBRARY_TEMPLATE = """
-genrule {
-    name: "create_directory_${local_module}",
-    cmd: "mkdir -p $$(genDir)/${local_module_path}",
-    out: ["dummy_file"],
-}
-
 cc_prebuilt_library_shared {
     name: "${local_module}",
     srcs: ["./${local_src_files}"],
-    suffix: ".so",
-    relative_install_path: "${local_module_path}",
     prefer: true,
     overrides: ["${local_overrides}"],
+    check_elf_files: false,
 }
 """
 
