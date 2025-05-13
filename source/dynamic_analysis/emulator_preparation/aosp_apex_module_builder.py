051ac2e67297f68eb506e602d3eaf6708a6b98df
Thomas Sutter <sutter.thomas@protonmail.com>
2025-05-13 16:05:24 +0200

    Fix capex file extraction for third layer

--- source/dynamic_analysis/emulator_preparation/aosp_apex_module_builder.py
+++ source/dynamic_analysis/emulator_preparation/aosp_apex_module_builder.py
@@ -16,5 +16,5 @@ def process_apex_files(firmware, destination_folder, store_setting_id, format_na
     filename_regex = r"\.(c?apex)$"
     search_pattern = re.compile(filename_regex, re.IGNORECASE)
     source_folder = get_firmware_export_folder_root(store_setting_id, firmware)
-    logging.debug(f"Processing shared libraries in {source_folder}")
+    logging.debug(f"Processing apex in {source_folder}")
     create_modules(source_folder, destination_folder, format_name, search_pattern, create_template_string)
