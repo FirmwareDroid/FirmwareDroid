ac9693234c77c1ce20179da6bafaa70935e858fb
Thomas Sutter <sutter.thomas@protonmail.com>
2025-08-19 09:36:43 +0200

    Fix query for build properties

--- source/api/v2/schema/BuildPropFileSchema.py
+++ source/api/v2/schema/BuildPropFileSchema.py
@@ -1,6 +1,8 @@
 # -*- coding: utf-8 -*-
 # This file is part of FirmwareDroid - https://github.com/FirmwareDroid/FirmwareDroid/blob/main/LICENSE.md
 # See the file 'LICENSE' for copying permission.
+import logging
+
 import django_rq
 import graphene
 from graphene.relay import Node
@@ -8,13 +10,18 @@ from graphene_mongo import MongoengineObjectType
 from graphql_jwt.decorators import superuser_required
 from api.v2.types.GenericFilter import generate_filter, get_filtered_queryset
 from model.BuildPropFile import BuildPropFile
+from graphene.types.generic import GenericScalar
 
-ModelFilter = generate_filter(BuildPropFile)
+class ExtendedBuildPropFileFilter(generate_filter(BuildPropFile)):
+    property_keys = graphene.List(graphene.String, description="Filter by property keys")
+    property_values = graphene.List(GenericScalar, description="Filter by property values")
+
+ModelFilter = ExtendedBuildPropFileFilter
 
 
 class BuildPropFileType(MongoengineObjectType):
     pk = graphene.String(source='pk')
-    property_values = graphene.JSONString(keys=graphene.List(graphene.String, required=True))
+    property_values = graphene.JSONString()
     property_keys = graphene.List(graphene.String)
 
     class Meta:
@@ -22,10 +29,9 @@ class BuildPropFileType(MongoengineObjectType):
         interfaces = (Node,)
         exclude_fields = ['build_prop_file']
 
-    def resolve_property_value(self, info, keys):
-        if not isinstance(keys, list):
-            raise ValueError("The 'keys' argument must be a list.")
-        return {key: self.properties.get(key, None) for key in keys}
+    def resolve_property_values(self, info):
+        return list(self.properties.values()) if self.properties else []
+
 
     def resolve_property_keys(self, info):
         return list(self.properties.keys()) if self.properties else []
@@ -40,4 +46,28 @@ class BuildPropFileQuery(graphene.ObjectType):
 
     @superuser_required
     def resolve_build_prop_file_id_list(self, info, object_id_list=None, field_filter=None):
-        return get_filtered_queryset(BuildPropFile, object_id_list, field_filter)
+        filter_dict = {}
+        key_list = None
+        value_list = None
+
+        if field_filter:
+            for k, v in field_filter.__dict__.items():
+                if not k.startswith('_') and v is not None:
+                    if k == "property_keys":
+                        key_list = v
+                        for key in v:
+                            filter_dict[f"properties__{key}__exists"] = True
+                    elif k == "property_values":
+                        value_list = v
+                    else:
+                        filter_dict[k] = v
+        queryset = get_filtered_queryset(BuildPropFile, object_id_list, filter_dict)
+
+        # Filter by property_values
+        if value_list:
+            def has_matching_value(doc):
+                return any(val in doc.properties.values() for val in value_list)
+
+            queryset = [doc for doc in queryset if has_matching_value(doc)]
+
+        return queryset
