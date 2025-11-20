f4486934d74e008ce5d6f46a62d7efc9ecfea097
Thomas Sutter <sutter.thomas@protonmail.com>
2025-11-20 11:43:28 +0100

    Migrate to eclipse-temurin since openjdk is deprecated. Add --serial option for the build script

--- docker/build_docker_images.sh
+++ docker/build_docker_images.sh
@@ -1,38 +1,90 @@
 #!/bin/bash
-
 # Script that builds all the base dockerfiles for docker-compose
+
 #####################################
-#Frontend                           #
+# Parse Arguments                   #
+#####################################
+set -o errexit
+set -o nounset
+set -o pipefail
+
+# Parse args
+SERIAL=false
+while [[ $# -gt 0 ]]; do
+  case "$1" in
+    -s|--serial)
+      SERIAL=true
+      shift
+      ;;
+    *)
+      echo "Usage: $0 [--serial|-s]"
+      exit 1
+      ;;
+  esac
+done
+
+#####################################
+# Frontend                           #
 #####################################
 docker build ./firmware-droid-client/ -f ./firmware-droid-client/Dockerfile -t firmwaredroid-frontend --platform="linux/amd64"
 retVal=$?
 if [ $retVal -ne 0 ]; then
-    echo "Error"
+    echo "Error building frontend"
     exit $retVal
 fi
 
 #####################################
-#Backend                            #
+# Backend                            #
 #####################################
 # Worker base
-printf "Building FirmwareDroid BASE IMAGE"
+printf "Building FirmwareDroid BASE IMAGE\n"
 docker build ./ -f ./Dockerfile_BASE -t firmwaredroid-base --platform="linux/amd64"
 retVal=$?
 if [ $retVal -ne 0 ]; then
-    echo "Error"
+    echo "Error building base image"
     exit $retVal
 fi
 
 # Build worker images
 workers=(./docker/base/Dockerfile_*)
-echo "Building worker images. This is gonna take some time..."
-for i in "${workers[@]}"; do
-  name=${i#*_}
-  echo "Building image for: ""$name"
-  docker build ./docker/base/ -f ./docker/base/Dockerfile_"$name" -t "$name"-worker --platform="linux/amd64" &
-done
+if [ "${#workers[@]}" -eq 0 ]; then
+  echo "No worker Dockerfiles found in ./docker/base/"
+else
+  echo "Building worker images. This may take some time..."
+  pids=()
+  for i in "${workers[@]}"; do
+    name=${i#*_}
+    echo "Building image for: $name"
+    if [ "$SERIAL" = true ]; then
+      echo "Building worker: $name"
+      docker build ./docker/base/ -f "./docker/base/Dockerfile_$name" -t "$name"-worker --platform="linux/amd64"
+      retVal=$?
+      if [ $retVal -ne 0 ]; then
+        echo "Error building worker: $name"
+        exit $retVal
+      fi
+    else
+      docker build ./docker/base/ -f "./docker/base/Dockerfile_$name" -t "$name"-worker --platform="linux/amd64" &
+      pids+=($!)
+    fi
+  done
+
+  if [ "$SERIAL" = false ]; then
+    # Wait for parallel builds to finish and fail on first non-zero exit
+    for pid in "${pids[@]}"; do
+      wait "$pid" || {
+        echo "One of the worker builds failed (pid $pid)"
+        exit 1
+      }
+    done
+  fi
+fi
+
+docker compose build
+retVal=$?
 if [ $retVal -ne 0 ]; then
-  echo "Error"
+  echo "Error running docker compose build"
   exit $retVal
 fi
-docker compose build
+
+echo "All builds completed successfully."
